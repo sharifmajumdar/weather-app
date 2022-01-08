d5be9cb139912963bb0bbeea3775462b21080638
sharifmajumdar <sharifmajumdar@gmail.com>
2022-01-08 19:35:51 +0200

    weather API has been added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,65 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import UserInput from './components/UserInput';
 import Card from './components/Card';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [name, setName ] = useState('');
+  const [info, setInfo ] = useState();
+
+  const getLocationKey = () => {
+    const apiKey = 'djAtrYtumaSnqwYQuUvQyEonYHQL4ZYm';
+    const locationUrl = `http://dataservice.accuweather.com/locations/v1/cities/search?q=${name}&apikey=${apiKey}`;
+    fetch(locationUrl)
+      .then(response => {
+        return response.json();
+      })
+      .then(res => res.find(el=>el.Country.ID === 'BD'))
+      .then(res => {
+        setInfo({
+          name: res.LocalizedName,
+          key: res.Key,
+        })
+      })
+  };
+
+  useEffect(()=>{
+    if(info) {
+      const apiKey = 'djAtrYtumaSnqwYQuUvQyEonYHQL4ZYm';
+      const locationKey = info && info.key;
+      const weatherApiUrl = `http://dataservice.accuweather.com/forecasts/v1/daily/1day/${locationKey}?apikey=${apiKey}` ;
+      fetch(weatherApiUrl)
+        .then(response => {
+          return response.json()
+        })
+        .then(res => {
+          console.log(res);
+          setData(res.DailyForecasts.map(el =>{
+            return {
+              min: el.Temperature.Minimum.Value,
+              max: el.Temperature.Maximum.Value,
+              date: el.Date,
+              dayCondition: el.Day.IconPhrase,
+              nightCondition: el.Night.IconPhrase,
+              source: el.Sources,
+            }
+          }))
+        })
+    }
+  }, [info]);
+
   return (
     <div className="App">
-      <UserInput />
-      <Card />
+      <div id="input-area">
+        <h1 class="text-center">Welcome to Weather App</h1>
+        <div id="submit-area">
+            <input type="text" id="city-name" placeholder="Enter your city name for Bangladesh like Dhaka" onChange = {(e)=> setName(e.target.value)} />
+            <button id="submit-button" onClick={getLocationKey}>Show</button>
+        </div>
+     </div>
+      {data && data.map(el=>
+        <Card forecastData = {el}  />
+        )}
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,17 @@
-import './style.css';
-function Card(){
-    return(
+import React from 'react';
+
+const Card = ({forecastData}) => {
+    return (
         <div id='card-area'>
-            <h1>Current Weather Info</h1>
-            <div id='info-area'>
-                <p>City Name</p>
-                <h3>Temperature</h3>
-            </div>
-        </div>
-    );
+        <h1>Current Weather Info</h1>
+        <span id ="date"><b>Today: </b>{forecastData.date}</span> <br></br>
+        <span id ="day-condition"><b>Day's Condition:</b> {forecastData.dayCondition}</span> <br></br>
+        <span id ="night-condition"><b>Night's Condition:</b> {forecastData.nightCondition}</span> <br></br>
+        <span id ="min-temp"><b>Min Temperature:</b> {forecastData.min}°F</span> <br></br>
+        <span id ="max-temp"><b>Max Temperature:</b> {forecastData.max}°F</span> <br></br>
+        <span id ="source"><b>Info Source:</b> <a href='https://www.accuweather.com/' target="_blank">{forecastData.source}</a> </span> <br></br>  
+    </div>
+    )
 }
 
 export default Card;
