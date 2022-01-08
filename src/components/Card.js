f522bfc4acf4996bdf52dce81a50e453ea13bbbf
sharifmajumdar <sharifmajumdar@gmail.com>
2022-01-08 20:33:08 +0200

    run yarn build for netlify deploy

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({forecastData}) => {
     return (
         <div id='card-area'>
-        <h1>Current Weather Info</h1>
+        <h1>Today's Weather Info</h1>
         <span id ="date"><b>Today: </b>{forecastData.date}</span> <br></br>
         <span id ="day-condition"><b>Day's Condition:</b> {forecastData.dayCondition}</span> <br></br>
         <span id ="night-condition"><b>Night's Condition:</b> {forecastData.nightCondition}</span> <br></br>
